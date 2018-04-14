0051e5a9df7e01a948f6fa719e5529da790d15a1
h4nter <hanterm2@mail.ru>
2018-04-14 14:28:29 +0300

    edited project (95%)

--- app/js/main.js
+++ app/js/main.js
@@ -12,27 +12,6 @@ $(function(){
         slidesToShow: 3,
         prevArrow: $('.post_prev'),
         nextArrow: $('.post_next'),
-//        responsive: [
-//            {
-//                breakpoint: 768,
-//                settings: {
-//                    arrows: false,
-//                    centerMode: true,
-//                    centerPadding: '40px',
-//                    slidesToShow: 3
-//                }
-//            },
-//            {
-//                breakpoint: 480,
-//                settings: {
-//                    arrows: false,
-//                    centerMode: true,
-//                    centerPadding: '40px',
-//                    slidesToShow: 1
-//                }
-//            }
-//        ]
-
     });
 
     var mixer = mixitup('.mixer');
@@ -54,4 +33,28 @@ $(function(){
             }
         }
     });
+
+    $('.btn_menu').click(function(){
+        $('.menu ul').slideToggle();
+    });
+});
+
+$("#menu, #btn_top").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+
+$(function(){
+ $("#btn_top").hide().removeAttr("href");
+ if ($(window).scrollTop()>="350") $("#btn_top").fadeIn("slow")
+ $(window).scroll(function(){
+  if ($(window).scrollTop()<="350") $("#btn_top").fadeOut("slow")
+  else $("#btn_top").fadeIn("slow")
+ });
 });
